10d31784185da2dc10ad0abec4a4fb85306379e8
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-05-25 17:39:14 +0900

    ✨ feat :: 크롤러 스크래핑 추가

--- crawler.js
+++ crawler.js
@@ -7,24 +7,21 @@ async function scrapeDynamicContent(url) {
     });
     const page = await browser.newPage();
 
-    // CSP 우회
     await page.setBypassCSP(true);
 
-    // 보다 현실적인 사용자 에이전트 설정
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
 
     await page.goto(url, { waitUntil: 'networkidle2' });
 
     await page.waitForSelector('#sidoCd');
-    await page.select('#sidoCd', '11'); // '서울특별시' 선택
-    await page.waitForTimeout(1000); // JavaScript가 반응할 시간 제공
+    await page.select('#sidoCd', 11);// 서울 특별시
+    await page.waitForTimeout(1000);
 
-    // 시/군/구 선택을 위해 필요한 JavaScript 함수를 호출할 수 있습니다.
     await page.evaluate(() => sggList());
 
     await page.waitForSelector('#sggCd');
-    await page.select('#sggCd', '성동구');
-    await page.waitForTimeout(1000); // JavaScript가 반응할 시간 제공
+    await page.select('#sggCd', '11200');
+    await page.waitForTimeout(1000);
 
     await page.waitForSelector('input.sch-btn');
     await page.click('input.sch-btn');
